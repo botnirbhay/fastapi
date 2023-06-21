b0ac22fab0be60cdf3e33acacc6c7cf931de795c
Nirbhay <hanjuranirbhay@gmail.com>
2023-06-22 00:37:21 +0530

    connected to db

--- blog-env/blog/main.py
+++ blog-env/blog/main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
-from . import schemas,models,database
+from fastapi import FastAPI,Depends,Response,status
+from . import schemas,models
+from .database import engine,SessionLocal
+from sqlalchemy.orm import Session
 
 
+models.Base.metadata.create_all(engine)
 
-models.Base.metadata.create_all(database.engine)
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 app=FastAPI()
 
 
-@app.post("/blog")
-def create(request:schemas.Blog):
-    return  request
+@app.post("/blog",status_code=201)
+def create(request:schemas.Blog,db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs=db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}',status_code=200)
+def getblog(id,response:Response,db:Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        response.status_code=status.HTTP_404_NOT_FOUND
+    return blog

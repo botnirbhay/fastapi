f6de0f38e775d5157c1b7bb23945a14eca2b8349
Nirbhay <hanjuranirbhay@gmail.com>
2023-06-23 11:08:30 +0530

    user routes added

--- blog-env/blog/main.py
+++ blog-env/blog/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI,Depends, HTTPException,Response,status
 from . import schemas,models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
+from passlib.context import CryptContext
 
 
 models.Base.metadata.create_all(engine)
@@ -16,8 +18,10 @@ def get_db():
 
 app=FastAPI()
 
+pwd_cxt=CryptContext(schemes=["bcrypt"],deprecated="auto")
 
-@app.post("/blog",status_code=201)
+
+@app.post("/blog",status_code=201,tags=['blogs'])
 def create(request:schemas.Blog,db:Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -25,12 +29,12 @@ def create(request:schemas.Blog,db:Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog',response_model=List[schemas.ShowBlog],tags=['blogs'])
 def all(db:Session = Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}',status_code=200)
+@app.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog,tags=['blogs'])
 def getblog(id,response:Response,db:Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -38,14 +42,14 @@ def getblog(id,response:Response,db:Session = Depends(get_db)):
     return blog
 
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def destroy(id,db:Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 # for updating a existing thing use put method
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
 def update(id,request:schemas.Blog,db:Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
@@ -53,3 +57,22 @@ def update(id,request:schemas.Blog,db:Session = Depends(get_db)):
     blog.update(request.dict())
     db.commit()
     return 'updated'
+
+
+@app.post('/user',response_model=schemas.ShowUser,tags=['users'])
+def create_user(request:schemas.User,db:Session = Depends(get_db)):
+    hashedpw=pwd_cxt.hash(request.password)
+    new_user=models.User(name=request.name,email=request.email,password=hashedpw)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+def getuser(id:int,response:Response,db:Session = Depends(get_db)):
+    user=db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return user
+

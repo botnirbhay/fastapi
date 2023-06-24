ba98623bc8d82e5bb16b391520cb35348f35d9fd
Nirbhay <hanjuranirbhay@gmail.com>
2023-06-24 12:47:10 +0530

    repo

--- blog-env/blog/routers/blog.py
+++ blog-env/blog/routers/blog.py
@@ -6,15 +6,18 @@ from sqlalchemy.orm import Session
 
 
 
-router=APIRouter()
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blogs']
+)
 
-@router.get('/blog',response_model=List[schemas.ShowBlog],tags=['blogs'])
+@router.get('/',response_model=List[schemas.ShowBlog])
 def all(db:Session = Depends(database.get_db)):
      blogs=db.query(models.Blog).all()
      return blogs
 
 
-@router.post("/blog",status_code=201,tags=['blogs'])
+@router.post("/",status_code=201)
 def create(request:schemas.Blog,db:Session = Depends(database.get_db)):
     new_blog = models.Blog(title=request.title,body=request.body,user_id=1)
     db.add(new_blog)
@@ -24,7 +27,7 @@ def create(request:schemas.Blog,db:Session = Depends(database.get_db)):
 
 
 
-@router.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog,tags=['blogs'])
+@router.get('/{id}',status_code=200,response_model=schemas.ShowBlog)
 def getblog(id,db:Session = Depends(database.get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -32,14 +35,14 @@ def getblog(id,db:Session = Depends(database.get_db)):
     return blog
 
 
-@router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id,db:Session = Depends(database.get_db)):
     db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 # for updating a existing thing use put method
-@router.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
+@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id,request:schemas.Blog,db:Session = Depends(database.get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
--- blog-env/blog/routers/user.py
+++ blog-env/blog/routers/user.py
@@ -4,11 +4,14 @@ from .. import schemas, database, models
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 
-router=APIRouter()
+router = APIRouter(
+    prefix="/user",
+    tags=['users']
+)
 
 pwd_cxt=CryptContext(schemes=["bcrypt"],deprecated="auto")
 
-@router.post('/user',response_model=schemas.ShowUser,tags=['users'])
+@router.post('/',response_model=schemas.ShowUser,tags=['users'])
 def create_user(request:schemas.User,db:Session = Depends(database.get_db)):
     hashedpw=pwd_cxt.hash(request.password)
     new_user=models.User(name=request.name,email=request.email,password=hashedpw)
@@ -18,7 +21,7 @@ def create_user(request:schemas.User,db:Session = Depends(database.get_db)):
     return new_user
 
 
-@router.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+@router.get('/{id}',response_model=schemas.ShowUser,tags=['users'])
 def getuser(id:int,db:Session = Depends(database.get_db)):
     user=db.query(models.User).filter(models.User.id==id).first()
     if not user:

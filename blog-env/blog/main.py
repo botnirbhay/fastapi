5f70c3e1b0656b0b701adad60ab44d349263d94a
Nirbhay <hanjuranirbhay@gmail.com>
2023-06-24 13:31:51 +0530

    auth added

--- blog-env/blog/main.py
+++ blog-env/blog/main.py
@@ -4,14 +4,14 @@ from .database import engine,SessionLocal,get_db
 from sqlalchemy.orm import Session
 from typing import List
 from passlib.context import CryptContext
-from .routers import blog,user
+from .routers import blog,user,authentication
 
 
 
 app=FastAPI()
 models.Base.metadata.create_all(engine)
 
-
+app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
--- blog-env/blog/schemas.py
+++ blog-env/blog/schemas.py
@@ -31,3 +31,8 @@ class ShowBlog(BaseModel):
     class Config():
         orm_mode=True
 
+
+class Login(BaseModel):
+    username:str
+    password:str
+
